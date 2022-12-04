168a993a5307acc9beb1ca7cfdbff7c494d97ce7
Radu Ionita <raduionita91@gmail.com>
2022-12-04 15:43:20 +0200

    add all files

---
+++ src/test/java/com/coremaker/interview/InterviewApplicationTests.java
@@ -0,0 +1,17 @@
+package com.coremaker.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+  @Test
+  void dummyTest() {
+    Assert.isTrue(true);
+  }
+}
