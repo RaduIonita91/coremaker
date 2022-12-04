acf4466a3bb2610640a79c7093d2e272fe55a2be
Radu Ionita <raduionita91@gmail.com>
2022-12-04 15:42:17 +0200

    User singup and User login - demo project

---
+++ src/main/java/com/coremaker/interview/InterviewApplication.java
@@ -0,0 +1,13 @@
+package com.coremaker.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewApplication {
+
+  public static void main(String[] args) {
+    System.out.println("Security demo!");
+    SpringApplication.run(InterviewApplication.class, args);
+  }
+}
